e90e6932ecc1dd6cbff1b418ff55ac6c2350a4d0
Francisco <fsousa.jpa@gmail.com>
2024-08-19 12:59:30 -0300

    add calculo imc e finalizado o app

--- app/src/main/java/com/esdraz/aulacalculadoraimc/ResultadoActivity.kt
+++ app/src/main/java/com/esdraz/aulacalculadoraimc/ResultadoActivity.kt
@@ -1,12 +1,20 @@
 package com.esdraz.aulacalculadoraimc
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class ResultadoActivity : AppCompatActivity() {
+
+
+    private lateinit var textPeso: TextView
+    private lateinit var textAltura: TextView
+    private lateinit var textResultado: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,33 @@ class ResultadoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        textPeso = findViewById(R.id.text_peso)
+        textAltura = findViewById(R.id.text_altura)
+        textResultado = findViewById(R.id.text_resultado)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            val peso = bundle.getDouble("peso")
+            val altura = bundle.getDouble("altura")
+
+            textPeso.text = "Peso informado $peso Kg"
+            textAltura.text = "Altura informada $altura m"
+
+            val imc = peso / (altura * altura)
+
+            val resultado = if (imc < 18.5) {
+                "Baixo"
+            } else if (imc in 18.5..24.9) {
+                "Normal"
+            } else if (imc in 25.0 .. 29.9) {
+                "Sobrepeso"
+            } else {
+                "Obeso"
+            }
+
+            textResultado.text = resultado
         }
     }
 }
